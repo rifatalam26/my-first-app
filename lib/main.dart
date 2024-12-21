66d1cd76896bf862819dbd13173c0505efa2ec92
Md Rifat Alam <mdrftlm@gmail.com>
2024-12-21 23:30:51 +0600

    first app run=> AppBar

--- lib/main.dart
+++ lib/main.dart
@@ -5,15 +5,25 @@ void main() {
   runApp(MaterialApp(
     home: Scaffold(
         appBar: AppBar(
-          leading: Icon(Icons.menu,color: Colors.blue,),
+      leading: IconButton(
+        icon: Icon(
+          Icons.menu,
+        ),
+        onPressed: () {},
+      ),
       title: Text("Home"),
-      elevation: 0,
+      actions: [
+        IconButton(onPressed: () {}, icon: Icon(Icons.shopping_cart)),
+        IconButton(onPressed: () {}, icon: Icon(Icons.search))
+      ],
+      elevation: 5,
       shadowColor: Colors.red,
       backgroundColor: Colors.purple,
+      centerTitle: true,
       shape: RoundedRectangleBorder(
           borderRadius: BorderRadius.vertical(bottom: Radius.circular(30))),
-          actions: [Icon(Icons.shopping_cart),
-          Icon(Icons.search)],
+      foregroundColor: Colors.white,
     )),
+    debugShowCheckedModeBanner: false,
   ));
 }
