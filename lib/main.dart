19337b995afe4ecd8ba6f349a23155bee8adc3e6
Md Rifat Alam <mdrftlm@gmail.com>
2024-12-21 23:09:52 +0600

    first app run=> AppBar

--- lib/main.dart
+++ lib/main.dart
@@ -6,12 +6,14 @@ void main() {
     home: Scaffold(
         appBar: AppBar(
           leading: Icon(Icons.menu,color: Colors.blue,),
-      title: Text("RIFAT ALAM"),
+      title: Text("Home"),
       elevation: 0,
       shadowColor: Colors.red,
       backgroundColor: Colors.purple,
       shape: RoundedRectangleBorder(
           borderRadius: BorderRadius.vertical(bottom: Radius.circular(30))),
+          actions: [Icon(Icons.shopping_cart),
+          Icon(Icons.search)],
     )),
   ));
 }
