23179526b562a417fecd196c2b5f78f8564fc7a8
Md Rifat Alam <mdrftlm@gmail.com>
2024-12-22 00:14:02 +0600

    first app run=> AppBar

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/cupertino.dart';
+//import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -6,21 +6,21 @@ void main() {
     home: Scaffold(
         appBar: AppBar(
       leading: IconButton(
-        icon: Icon(
+        icon:const Icon(
           Icons.menu,
         ),
         onPressed: () {},
       ),
-      title: Text("Home"),
+      title:const Text("Home"),
       actions: [
-        IconButton(onPressed: () {}, icon: Icon(Icons.shopping_cart)),
-        IconButton(onPressed: () {}, icon: Icon(Icons.search))
+        IconButton(onPressed: () {}, icon:const Icon(Icons.shopping_cart)),
+        IconButton(onPressed: () {}, icon:const Icon(Icons.search))
       ],
       elevation: 5,
       shadowColor: Colors.red,
       backgroundColor: Colors.purple,
       centerTitle: true,
-      shape: RoundedRectangleBorder(
+      shape:const RoundedRectangleBorder(
           borderRadius: BorderRadius.vertical(bottom: Radius.circular(30))),
       foregroundColor: Colors.white,
     )),
