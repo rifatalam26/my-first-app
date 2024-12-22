e2213f64091c0093aeaae0435434cb5d8475abbb
Md Rifat Alam <mdrftlm@gmail.com>
2024-12-22 23:24:39 +0600

    AppBar

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,25 @@
-//import 'package:flutter/cupertino.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
     home: Scaffold(
-        appBar: AppBar(
-      leading: IconButton(
-        icon: const Icon(
-          Icons.menu,
-        ),
-        onPressed: () {},
+      appBar: AppBar(
+        leading: IconButton(onPressed: () {}, icon: const Icon(Icons.menu)),
+        title: const Text("Home"),
+        actions: [
+          IconButton(onPressed: () {}, icon: const Icon(Icons.shopping_cart)),
+          IconButton(onPressed: () {}, icon: const Icon(Icons.search)),
+        ],
+        backgroundColor: Colors.purple,
+        centerTitle: true,
+        foregroundColor: Colors.white,
+        elevation: 5,
+        shadowColor: Colors.red,
+        shape: const RoundedRectangleBorder(
+            borderRadius: BorderRadius.vertical(bottom: Radius.circular(30))),
       ),
-      title: const Text("Home"),
-      actions: [
-        IconButton(onPressed: () {}, icon: const Icon(Icons.shopping_cart)),
-        IconButton(onPressed: () {}, icon: const Icon(Icons.search))
-      ],
-      elevation: 5,
-      shadowColor: Colors.red,
-      backgroundColor: Colors.purple,
-      centerTitle: true,
-      shape: const RoundedRectangleBorder(
-          borderRadius: BorderRadius.vertical(bottom: Radius.circular(30))),
-      foregroundColor: Colors.white,
-    )),
+    ),
     debugShowCheckedModeBanner: false,
   ));
 }
