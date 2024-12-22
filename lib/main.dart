ddacd1eed9a46700e6ae57bee53a5069848b292d
Md Rifat Alam <mdrftlm@gmail.com>
2024-12-22 23:35:22 +0600

    StatelessWidget

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,14 @@
 import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-    home: Scaffold(
-      appBar: AppBar(
-        leading: IconButton(onPressed: () {}, icon: const Icon(Icons.menu)),
-        title: const Text("Home"),
-        actions: [
-          IconButton(onPressed: () {}, icon: const Icon(Icons.shopping_cart)),
-          IconButton(onPressed: () {}, icon: const Icon(Icons.search)),
-        ],
-        backgroundColor: Colors.purple,
-        centerTitle: true,
-        foregroundColor: Colors.white,
-        elevation: 5,
-        shadowColor: Colors.red,
-        shape: const RoundedRectangleBorder(
-            borderRadius: BorderRadius.vertical(bottom: Radius.circular(30))),
-      ),
-    ),
-    debugShowCheckedModeBanner: false,
-  ));
+  runApp(const MaterialApp());
 }
+class MaterialApp extends StatelessWidget {
+  const MaterialApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
+
