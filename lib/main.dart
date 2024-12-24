274196dc4a9f6cabbd231713db6a3d08be77bca6
Md Rifat Alam <mdrftlm@gmail.com>
2024-12-24 21:51:37 +0600

    Row Practice

--- lib/main.dart
+++ lib/main.dart
@@ -28,26 +28,35 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        leading: IconButton(onPressed: () {}, icon: Icon(Icons.menu)),
-        title: Text("Home",
-            style: TextStyle(
-              color: Colors.white,
-              fontSize:25
-            )),
-        actions: [
-          IconButton(onPressed: () {}, icon: const Icon(Icons.shopping_cart)),
-          IconButton(onPressed: () {}, icon: const Icon(Icons.search)),
-        ],
-        centerTitle: true,
-        backgroundColor: Colors.purple,
-        foregroundColor: Colors.white,
-        shadowColor: Colors.red,
-        elevation: 5,
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.vertical(bottom: Radius.circular(30)),
+        appBar: AppBar(
+          leading: IconButton(onPressed: () {}, icon: Icon(Icons.menu)),
+          title:
+              Text("Home", style: TextStyle(color: Colors.white, fontSize: 25)),
+          actions: [
+            IconButton(onPressed: () {}, icon: const Icon(Icons.shopping_cart)),
+            IconButton(onPressed: () {}, icon: const Icon(Icons.search)),
+          ],
+          centerTitle: true,
+          backgroundColor: Colors.purple,
+          foregroundColor: Colors.white,
+          shadowColor: Colors.red,
+          elevation: 5,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.vertical(bottom: Radius.circular(30)),
+          ),
         ),
-      ),
-    );
+        body: Container(
+         color:  Colors.purple,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text("Riiiiiiiiiiiiiiiiiiifat"),
+              Text("Alam"),
+              Icon(Icons.account_circle_sharp)
+            ],
+          )
+          ,
+        ));
   }
 }
