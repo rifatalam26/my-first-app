c6e6ab11d491d3f5b45a97aa15353383ebe54c3f
Md Rifat Alam <mdrftlm@gmail.com>
2024-12-25 23:41:03 +0600

    container Stack practice

--- lib/main.dart
+++ lib/main.dart
@@ -28,53 +28,53 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          leading: IconButton(onPressed: () {}, icon: Icon(Icons.menu)),
-          title:
-              Text("Home", style: TextStyle(color: Colors.white, fontSize: 25)),
-          actions: [
-            IconButton(onPressed: () {}, icon: const Icon(Icons.shopping_cart)),
-            IconButton(onPressed: () {}, icon: const Icon(Icons.search)),
-          ],
-          centerTitle: true,
-          backgroundColor: Colors.purple,
-          foregroundColor: Colors.white,
-          shadowColor: Colors.red,
-          elevation: 5,
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.vertical(bottom: Radius.circular(30)),
-          ),
+      appBar: AppBar(
+        leading: IconButton(onPressed: () {}, icon: Icon(Icons.menu)),
+        title:
+            Text("Home", style: TextStyle(color: Colors.white, fontSize: 25)),
+        actions: [
+          IconButton(onPressed: () {}, icon: const Icon(Icons.shopping_cart)),
+          IconButton(onPressed: () {}, icon: const Icon(Icons.search)),
+        ],
+        centerTitle: true,
+        backgroundColor: Colors.purple,
+        foregroundColor: Colors.white,
+        shadowColor: Colors.red,
+        elevation: 5,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.vertical(bottom: Radius.circular(30)),
         ),
-        body: Stack(
-          children: [
-            Container(
-                padding: EdgeInsets.all(20),
-                margin: EdgeInsets.all(20),
-                height: 300,
-                width: 300,
-                decoration: BoxDecoration(
-                  border: Border(
-                      top: BorderSide(color: Colors.red, width: 15),
-                      bottom: BorderSide(color: Colors.blue, width: 15),
-                      right: BorderSide(color: Colors.green, width: 15),
-                      left: BorderSide(color: Colors.amber, width: 15)),
-                  color: Colors.deepPurple,
-                ),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Center(
-                      child: Icon(Icons.account_circle_sharp),
-                    ),
-                  ],
-                )),
-            Positioned(left: 60, top: 60, child: Icon(Icons.add_a_photo_sharp)),
-            Positioned(
-                left: 60, bottom: 60, child: Icon(Icons.ac_unit_outlined)),
-            Positioned(bottom: 60, right: 60, child: Icon(Icons.access_alarm)),
-            Positioned(top: 60, right: 60, child: Icon(Icons.accessible)),
-          ],
-        ));
+      ),
+      body: Stack(
+        children: [
+          Container(
+              padding: EdgeInsets.all(20),
+              margin: EdgeInsets.all(20),
+              height: 300,
+              width: 300,
+              decoration: BoxDecoration(
+                border: Border(
+                    top: BorderSide(color: Colors.red, width: 15),
+                    bottom: BorderSide(color: Colors.blue, width: 15),
+                    right: BorderSide(color: Colors.green, width: 15),
+                    left: BorderSide(color: Colors.amber, width: 15)),
+                color: Colors.deepPurple,
+              ),
+              child: const Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Center(
+                    child: Icon(Icons.account_circle_sharp),
+                  ),
+                ],
+              )),
+          Positioned(left: 60, top: 60, child: Icon(Icons.add_a_photo_sharp)),
+          Positioned(left: 60, bottom: 60, child: Icon(Icons.ac_unit_outlined)),
+          Positioned(bottom: 60, right: 60, child: Icon(Icons.access_alarm)),
+          Positioned(top: 60, right: 60, child: Icon(Icons.accessible)),
+        ],
+      )
+    );
   }
 }
