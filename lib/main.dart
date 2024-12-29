101fd8dd894c8985d69365181142d52bf0d51051
Md Rifat Alam <mdrftlm@gmail.com>
2024-12-29 22:44:00 +0600

    Text field widget

--- lib/main.dart
+++ lib/main.dart
@@ -55,132 +55,141 @@ class _HomePageState extends State<HomePage> {
           ),
         ),
       ),
-      body: Padding(
-          padding: EdgeInsets.all(8),
-          child: Column(
-            children: [
-              Container(
-                height: 100,
-               // color: Color(0xff4c5885),
-                decoration: BoxDecoration(
-                  image: DecorationImage(
-                    fit: BoxFit.fill,
-                      filterQuality:FilterQuality.high ,
+      // body: Padding(
+      //     padding: EdgeInsets.all(8),
+      //     child: Column(
+      //       children: [
+      //         Container(
+      //           height: 100,
+      //           // color: Color(0xff4c5885),
+      //           decoration: BoxDecoration(
+      //               image: DecorationImage(
+      //                   fit: BoxFit.fill,
+      //                   filterQuality: FilterQuality.high,
+      //                   image: AssetImage(
+      //                       "images/Screenshot_2024-02-22-21-26-05-74.jpg"))),
+      //         ),
+      //         SizedBox(
+      //           height: 10,
+      //         ),
+      //         Row(
+      //           mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      //           children: [
+      //             Column(children: [
+      //               Container(
+      //                 height: 100,
+      //                 width: 100,
+      //                 color: Color(0xff4c5885),
+      //                 child: Image.asset(
+      //                     "images/Screenshot_2024-02-22-21-26-05-74.jpg"),
+      //               ),
+      //               SizedBox(
+      //                 height: 10,
+      //               ),
+      //               Container(
+      //                 height: 100,
+      //                 width: 100,
+      //                 color: Color(0xff4c5885),
+      //               ),
+      //               SizedBox(
+      //                 height: 10,
+      //               ),
+      //               Container(
+      //                 height: 100,
+      //                 width: 100,
+      //                 color: Color(0xff4c5885),
+      //               ),
+      //               SizedBox(
+      //                 height: 10,
+      //               ),
+      //               Container(
+      //                 height: 100,
+      //                 width: 100,
+      //                 color: Color(0xff4c5885),
+      //               ),
+      //               SizedBox(
+      //                 height: 10,
+      //               )
+      //             ]),
+      //             Column(children: [
+      //               Container(
+      //                 height: 100,
+      //                 width: 100,
+      //                 color: Color(0xff4c5885),
+      //               ),
+      //               SizedBox(
+      //                 height: 10,
+      //               ),
+      //               Container(
+      //                 height: 100,
+      //                 width: 100,
+      //                 color: Color(0xff4c5885),
+      //               ),
+      //               SizedBox(
+      //                 height: 10,
+      //               ),
+      //               Container(
+      //                 height: 100,
+      //                 width: 100,
+      //                 color: Color(0xff4c5885),
+      //               ),
+      //               SizedBox(
+      //                 height: 10,
+      //               ),
+      //               Container(
+      //                 height: 100,
+      //                 width: 100,
+      //                 color: Color(0xff4c5885),
+      //               ),
+      //               SizedBox(
+      //                 height: 10,
+      //               )
+      //             ]),
+      //             Column(children: [
+      //               Container(
+      //                 height: 100,
+      //                 width: 100,
+      //                 color: Color(0xff4c5885),
+      //               ),
+      //               SizedBox(
+      //                 height: 10,
+      //               ),
+      //               Container(
+      //                 height: 100,
+      //                 width: 100,
+      //                 color: Color(0xff4c5885),
+      //               ),
+      //               SizedBox(
+      //                 height: 10,
+      //               ),
+      //               Container(
+      //                 height: 100,
+      //                 width: 100,
+      //                 color: Color(0xff4c5885),
+      //               ),
+      //               SizedBox(
+      //                 height: 10,
+      //               ),
+      //               Container(
+      //                 height: 100,
+      //                 width: 100,
+      //                 color: Color(0xff4c5885),
+      //               ),
+      //             ]),
+      //           ],
+      //         )
+      //       ],
+      //     )),
+      body: Padding(padding: EdgeInsets.symmetric(horizontal: 30),
+      child:Center(
+        child: TextField(
+          decoration: InputDecoration(
+            border: InputBorder.none,
+          ),
+        ),
+      ),
 
-                      image: AssetImage("images/Screenshot_2024-02-22-21-26-05-74.jpg")
-                  )
-                ),
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Column(children: [
-                    Container(
-                      height: 100,
-                      width: 100,
-                      color: Color(0xff4c5885),
-                      child: Image.asset("images/Screenshot_2024-02-22-21-26-05-74.jpg"),
-                    ),
-                    SizedBox(
-                      height: 10,
-                    ),
-                    Container(
-                      height: 100,
-                      width: 100,
-                      color: Color(0xff4c5885),
-                    ),
-                    SizedBox(
-                      height: 10,
-                    ),
-                    Container(
-                      height: 100,
-                      width: 100,
-                      color: Color(0xff4c5885),
-                    ),
-                    SizedBox(
-                      height: 10,
-                    ),
-                    Container(
-                      height: 100,
-                      width: 100,
-                      color: Color(0xff4c5885),
-                    ),
-                    SizedBox(
-                      height: 10,
-                    )
-                  ]),
-                  Column(children: [
-                    Container(
-                      height: 100,
-                      width: 100,
-                      color: Color(0xff4c5885),
-                    ),
-                    SizedBox(
-                      height: 10,
-                    ),
-                    Container(
-                      height: 100,
-                      width: 100,
-                      color: Color(0xff4c5885),
-                    ),
-                    SizedBox(
-                      height: 10,
-                    ),
-                    Container(
-                      height: 100,
-                      width: 100,
-                      color: Color(0xff4c5885),
-                    ),
-                    SizedBox(
-                      height: 10,
-                    ),
-                    Container(
-                      height: 100,
-                      width: 100,
-                      color: Color(0xff4c5885),
-                    ),
-                    SizedBox(
-                      height: 10,
-                    )
-                  ]),
-                  Column(children: [
-                    Container(
-                      height: 100,
-                      width: 100,
-                      color: Color(0xff4c5885),
-                    ),
-                    SizedBox(
-                      height: 10,
-                    ),
-                    Container(
-                      height: 100,
-                      width: 100,
-                      color: Color(0xff4c5885),
-                    ),
-                    SizedBox(
-                      height: 10,
-                    ),
-                    Container(
-                      height: 100,
-                      width: 100,
-                      color: Color(0xff4c5885),
-                    ),
-                    SizedBox(
-                      height: 10,
-                    ),
-                    Container(
-                      height: 100,
-                      width: 100,
-                      color: Color(0xff4c5885),
-                    ),
-                  ]),
-                ],
-              )
-            ],
-          )),
+      ),
     );
   }
 }
