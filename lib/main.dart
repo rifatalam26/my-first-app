e98c144ff890bbba7a2604c0ccb4a41c3184a458
Md Rifat Alam <mdrftlm@gmail.com>
2024-12-31 09:00:31 +0600

    App design

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,10 @@
-
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-void main(){
-
+void main() {
   runApp(MyApp());
-
 }
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
@@ -19,6 +16,7 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
@@ -31,11 +29,11 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        leading: IconButton(onPressed: (){}, icon: Icon(Icons.menu)),
+        leading: IconButton(onPressed: () {}, icon: Icon(Icons.menu)),
         title: Text("Home"),
         actions: [
-          IconButton(onPressed: (){}, icon: Icon(Icons.shopping_cart)),
-          IconButton(onPressed: (){}, icon: Icon(Icons.search)),
+          IconButton(onPressed: () {}, icon: Icon(Icons.shopping_cart)),
+          IconButton(onPressed: () {}, icon: Icon(Icons.search)),
         ],
         backgroundColor: Colors.purple,
         centerTitle: true,
@@ -48,20 +46,113 @@ class _HomePageState extends State<HomePage> {
         padding: const EdgeInsets.all(25.0),
         child: Column(
           children: [
-             Container(
-                height: 80,
-                decoration: BoxDecoration(
-                  border: Border.all(color: Colors.blue,width: 4),
-                  borderRadius: BorderRadius.all(Radius.circular(40)),
-
-                ),
+            Container(
+              height: 80,
+              decoration: BoxDecoration(
+                border: Border.all(color: Colors.blue, width: 4),
+                borderRadius: const BorderRadius.all(Radius.circular(40)),
               ),
-
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Icon(
+                    Icons.shop,
+                    size: 50,
+                    color: Colors.blue,
+                  ),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "Item One",
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 20),
+                      ),
+                      Text("Border Station"),
+                    ],
+                  ),
+                  Icon(
+                    Icons.play_arrow,
+                    size: 50,
+                    color: Colors.blue,
+                  )
+                ],
+              ),
+            ),
+            SizedBox(height: 15,),
+            Container(
+              height: 80,
+              decoration: BoxDecoration(
+                border: Border.all(color: Colors.green, width: 4),
+               // borderRadius: const BorderRadius.all(Radius.circular(40)),
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Icon(
+                    Icons.run_circle,
+                    size: 50,
+                    color: Colors.blue,
+                  ),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "Item two",
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 20),
+                      ),
+                      Text("BeveledRactangleBorder"),
+                    ],
+                  ),
+                  Icon(
+                    Icons.play_arrow,
+                    size: 50,
+                    color: Colors.green,
+                  )
+                ],
+              ),
+            ),
+            SizedBox(height: 15,),
+            Container(
+              height: 80,
+              decoration: BoxDecoration(
+                border: Border.all(color: Colors.blue, width: 4),
+                borderRadius: const BorderRadius.all(Radius.circular(40)),
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Icon(
+                    Icons.shop,
+                    size: 50,
+                    color: Colors.blue,
+                  ),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "Item One",
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 20),
+                      ),
+                      Text("Border Station"),
+                    ],
+                  ),
+                  Icon(
+                    Icons.play_arrow,
+                    size: 50,
+                    color: Colors.blue,
+                  )
+                ],
+              ),
+            ),
           ],
         ),
       ),
     );
   }
 }
-
-
