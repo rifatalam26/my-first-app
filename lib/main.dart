95fa958c7984d18df5eaecfe6029e30b782583ec
Md Rifat Alam <mdrftlm@gmail.com>
2025-01-01 23:59:33 +0600

    card design

--- lib/main.dart
+++ lib/main.dart
@@ -50,6 +50,7 @@ class _HomePageState extends State<HomePage> {
           borderRadius: BorderRadius.all(Radius.circular(30)),
           borderSide: BorderSide(color: Colors.black,width: 3),
         ) ,
+
         child: Text("data"),
       )
     );
