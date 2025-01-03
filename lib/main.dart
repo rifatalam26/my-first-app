17602b9ec57b4984b274215bde2326984b1bbe63
Md Rifat Alam <mdrftlm@gmail.com>
2025-01-03 10:14:57 +0600

    ListView

--- lib/main.dart
+++ lib/main.dart
@@ -44,7 +44,7 @@ class _HomePageState extends State<HomePage> {
         ),
 
       body: Center(
-        child: Row(
+        child: ListView(
           children: [
             Card(
               color: Colors.blue,
@@ -57,8 +57,9 @@ class _HomePageState extends State<HomePage> {
                 width: 170,
                 child: Column(
                   children: [
-                   SizedBox(height: 130,
+                  SizedBox(height: 130,
                    child: Card(
+
                      elevation: 0,
                      shape: OutlineInputBorder(
                        borderRadius: BorderRadius.only(topLeft: Radius.circular(30),topRight: Radius.circular(30)),
@@ -108,6 +109,346 @@ class _HomePageState extends State<HomePage> {
                     ),
                     SizedBox(height: 15,),
                     Text("News",style: TextStyle(color: Colors.white,fontSize: 25,fontWeight:FontWeight.bold ),)
+                  ],
+                ),
+              ),
+            ),
+            Card(
+              color: Colors.blue,
+              shape: OutlineInputBorder(
+                borderRadius: BorderRadius.all(Radius.circular(30)),
+                borderSide: BorderSide(color: Colors.transparent),
+              ),
+              child: SizedBox(
+                height: 200,
+                width: 170,
+                child: Column(
+                  children: [
+                    SizedBox(height: 130,
+                      child: Card(
+
+                        elevation: 0,
+                        shape: OutlineInputBorder(
+                            borderRadius: BorderRadius.only(topLeft: Radius.circular(30),topRight: Radius.circular(30)),
+                            borderSide: BorderSide(color: Colors.transparent)
+                        ),
+                        margin: EdgeInsets.zero,
+                        child: Row(mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Icon(Icons.person_outline,size: 70,color: Colors.blueAccent,),
+                            Text("2025",style: TextStyle(color: Colors.blueAccent,fontSize: 25.0,fontWeight: FontWeight.bold),)
+                          ],
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 15,),
+                    Text("Student",style: TextStyle(color: Colors.white,fontSize: 25,fontWeight:FontWeight.bold ),)
+                  ],
+                ),
+              ),
+            ),
+            Card(
+              color: Colors.blue,
+              shape: OutlineInputBorder(
+                borderRadius: BorderRadius.all(Radius.circular(30)),
+                borderSide: BorderSide(color: Colors.transparent),
+              ),
+              child: SizedBox(
+                height: 200,
+                width: 170,
+                child: Column(
+                  children: [
+                    SizedBox(height: 130,
+                      child: Card(
+
+                        elevation: 0,
+                        shape: OutlineInputBorder(
+                            borderRadius: BorderRadius.only(topLeft: Radius.circular(30),topRight: Radius.circular(30)),
+                            borderSide: BorderSide(color: Colors.transparent)
+                        ),
+                        margin: EdgeInsets.zero,
+                        child: Row(mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Icon(Icons.person_outline,size: 70,color: Colors.blueAccent,),
+                            Text("2025",style: TextStyle(color: Colors.blueAccent,fontSize: 25.0,fontWeight: FontWeight.bold),)
+                          ],
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 15,),
+                    Text("Student",style: TextStyle(color: Colors.white,fontSize: 25,fontWeight:FontWeight.bold ),)
+                  ],
+                ),
+              ),
+            ),
+            Card(
+              color: Colors.blue,
+              shape: OutlineInputBorder(
+                borderRadius: BorderRadius.all(Radius.circular(30)),
+                borderSide: BorderSide(color: Colors.transparent),
+              ),
+              child: SizedBox(
+                height: 200,
+                width: 170,
+                child: Column(
+                  children: [
+                    SizedBox(height: 130,
+                      child: Card(
+
+                        elevation: 0,
+                        shape: OutlineInputBorder(
+                            borderRadius: BorderRadius.only(topLeft: Radius.circular(30),topRight: Radius.circular(30)),
+                            borderSide: BorderSide(color: Colors.transparent)
+                        ),
+                        margin: EdgeInsets.zero,
+                        child: Row(mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Icon(Icons.person_outline,size: 70,color: Colors.blueAccent,),
+                            Text("2025",style: TextStyle(color: Colors.blueAccent,fontSize: 25.0,fontWeight: FontWeight.bold),)
+                          ],
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 15,),
+                    Text("Student",style: TextStyle(color: Colors.white,fontSize: 25,fontWeight:FontWeight.bold ),)
+                  ],
+                ),
+              ),
+            ),
+            Card(
+              color: Colors.blue,
+              shape: OutlineInputBorder(
+                borderRadius: BorderRadius.all(Radius.circular(30)),
+                borderSide: BorderSide(color: Colors.transparent),
+              ),
+              child: SizedBox(
+                height: 200,
+                width: 170,
+                child: Column(
+                  children: [
+                    SizedBox(height: 130,
+                      child: Card(
+
+                        elevation: 0,
+                        shape: OutlineInputBorder(
+                            borderRadius: BorderRadius.only(topLeft: Radius.circular(30),topRight: Radius.circular(30)),
+                            borderSide: BorderSide(color: Colors.transparent)
+                        ),
+                        margin: EdgeInsets.zero,
+                        child: Row(mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Icon(Icons.person_outline,size: 70,color: Colors.blueAccent,),
+                            Text("2025",style: TextStyle(color: Colors.blueAccent,fontSize: 25.0,fontWeight: FontWeight.bold),)
+                          ],
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 15,),
+                    Text("Student",style: TextStyle(color: Colors.white,fontSize: 25,fontWeight:FontWeight.bold ),)
+                  ],
+                ),
+              ),
+            ),
+            Card(
+              color: Colors.blue,
+              shape: OutlineInputBorder(
+                borderRadius: BorderRadius.all(Radius.circular(30)),
+                borderSide: BorderSide(color: Colors.transparent),
+              ),
+              child: SizedBox(
+                height: 200,
+                width: 170,
+                child: Column(
+                  children: [
+                    SizedBox(height: 130,
+                      child: Card(
+
+                        elevation: 0,
+                        shape: OutlineInputBorder(
+                            borderRadius: BorderRadius.only(topLeft: Radius.circular(30),topRight: Radius.circular(30)),
+                            borderSide: BorderSide(color: Colors.transparent)
+                        ),
+                        margin: EdgeInsets.zero,
+                        child: Row(mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Icon(Icons.person_outline,size: 70,color: Colors.blueAccent,),
+                            Text("2025",style: TextStyle(color: Colors.blueAccent,fontSize: 25.0,fontWeight: FontWeight.bold),)
+                          ],
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 15,),
+                    Text("Student",style: TextStyle(color: Colors.white,fontSize: 25,fontWeight:FontWeight.bold ),)
+                  ],
+                ),
+              ),
+            ),
+            Card(
+              color: Colors.blue,
+              shape: OutlineInputBorder(
+                borderRadius: BorderRadius.all(Radius.circular(30)),
+                borderSide: BorderSide(color: Colors.transparent),
+              ),
+              child: SizedBox(
+                height: 200,
+                width: 170,
+                child: Column(
+                  children: [
+                    SizedBox(height: 130,
+                      child: Card(
+
+                        elevation: 0,
+                        shape: OutlineInputBorder(
+                            borderRadius: BorderRadius.only(topLeft: Radius.circular(30),topRight: Radius.circular(30)),
+                            borderSide: BorderSide(color: Colors.transparent)
+                        ),
+                        margin: EdgeInsets.zero,
+                        child: Row(mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Icon(Icons.person_outline,size: 70,color: Colors.blueAccent,),
+                            Text("2025",style: TextStyle(color: Colors.blueAccent,fontSize: 25.0,fontWeight: FontWeight.bold),)
+                          ],
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 15,),
+                    Text("Student",style: TextStyle(color: Colors.white,fontSize: 25,fontWeight:FontWeight.bold ),)
+                  ],
+                ),
+              ),
+            ),
+            Card(
+              color: Colors.blue,
+              shape: OutlineInputBorder(
+                borderRadius: BorderRadius.all(Radius.circular(30)),
+                borderSide: BorderSide(color: Colors.transparent),
+              ),
+              child: SizedBox(
+                height: 200,
+                width: 170,
+                child: Column(
+                  children: [
+                    SizedBox(height: 130,
+                      child: Card(
+
+                        elevation: 0,
+                        shape: OutlineInputBorder(
+                            borderRadius: BorderRadius.only(topLeft: Radius.circular(30),topRight: Radius.circular(30)),
+                            borderSide: BorderSide(color: Colors.transparent)
+                        ),
+                        margin: EdgeInsets.zero,
+                        child: Row(mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Icon(Icons.person_outline,size: 70,color: Colors.blueAccent,),
+                            Text("2025",style: TextStyle(color: Colors.blueAccent,fontSize: 25.0,fontWeight: FontWeight.bold),)
+                          ],
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 15,),
+                    Text("Student",style: TextStyle(color: Colors.white,fontSize: 25,fontWeight:FontWeight.bold ),)
+                  ],
+                ),
+              ),
+            ),
+            Card(
+              color: Colors.blue,
+              shape: OutlineInputBorder(
+                borderRadius: BorderRadius.all(Radius.circular(30)),
+                borderSide: BorderSide(color: Colors.transparent),
+              ),
+              child: SizedBox(
+                height: 200,
+                width: 170,
+                child: Column(
+                  children: [
+                    SizedBox(height: 130,
+                      child: Card(
+
+                        elevation: 0,
+                        shape: OutlineInputBorder(
+                            borderRadius: BorderRadius.only(topLeft: Radius.circular(30),topRight: Radius.circular(30)),
+                            borderSide: BorderSide(color: Colors.transparent)
+                        ),
+                        margin: EdgeInsets.zero,
+                        child: Row(mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Icon(Icons.person_outline,size: 70,color: Colors.blueAccent,),
+                            Text("2025",style: TextStyle(color: Colors.blueAccent,fontSize: 25.0,fontWeight: FontWeight.bold),)
+                          ],
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 15,),
+                    Text("Student",style: TextStyle(color: Colors.white,fontSize: 25,fontWeight:FontWeight.bold ),)
+                  ],
+                ),
+              ),
+            ),
+            Card(
+              color: Colors.blue,
+              shape: OutlineInputBorder(
+                borderRadius: BorderRadius.all(Radius.circular(30)),
+                borderSide: BorderSide(color: Colors.transparent),
+              ),
+              child: SizedBox(
+                height: 200,
+                width: 170,
+                child: Column(
+                  children: [
+                    SizedBox(height: 130,
+                      child: Card(
+
+                        elevation: 0,
+                        shape: OutlineInputBorder(
+                            borderRadius: BorderRadius.only(topLeft: Radius.circular(30),topRight: Radius.circular(30)),
+                            borderSide: BorderSide(color: Colors.transparent)
+                        ),
+                        margin: EdgeInsets.zero,
+                        child: Row(mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Icon(Icons.person_outline,size: 70,color: Colors.blueAccent,),
+                            Text("2025",style: TextStyle(color: Colors.blueAccent,fontSize: 25.0,fontWeight: FontWeight.bold),)
+                          ],
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 15,),
+                    Text("Student",style: TextStyle(color: Colors.white,fontSize: 25,fontWeight:FontWeight.bold ),)
+                  ],
+                ),
+              ),
+            ),
+            Card(
+              color: Colors.blue,
+              shape: OutlineInputBorder(
+                borderRadius: BorderRadius.all(Radius.circular(30)),
+                borderSide: BorderSide(color: Colors.transparent),
+              ),
+              child: SizedBox(
+                height: 200,
+                width: 170,
+                child: Column(
+                  children: [
+                    SizedBox(height: 130,
+                      child: Card(
+
+                        elevation: 0,
+                        shape: OutlineInputBorder(
+                            borderRadius: BorderRadius.only(topLeft: Radius.circular(30),topRight: Radius.circular(30)),
+                            borderSide: BorderSide(color: Colors.transparent)
+                        ),
+                        margin: EdgeInsets.zero,
+                        child: Row(mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Icon(Icons.person_outline,size: 70,color: Colors.blueAccent,),
+                            Text("2025",style: TextStyle(color: Colors.blueAccent,fontSize: 25.0,fontWeight: FontWeight.bold),)
+                          ],
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 15,),
+                    Text("Student",style: TextStyle(color: Colors.white,fontSize: 25,fontWeight:FontWeight.bold ),)
                   ],
                 ),
               ),
