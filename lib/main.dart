c02f0c89d5047c415ca31701223d48568a540aa1
Md Rifat Alam <mdrftlm@gmail.com>
2025-01-14 19:56:22 +0600

    Elevated Button

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:my_app/view/home_screen.dart';
 
 void main() {
   runApp(MyApp());
@@ -26,125 +25,42 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  int c=0;
+  void counter(){
+    c++;
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        // leading: Icon(Icons.account_circle),
-        title: Text("Appbar"),
-        actions: [Icon(Icons.settings)],
-        backgroundColor: Colors.blue,
+        title: Text(
+          "Appber",
+          style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+        ),
+        actions: [Icon(Icons.account_circle_outlined)],
+        backgroundColor: Colors.indigoAccent,
         foregroundColor: Colors.white,
         centerTitle: true,
       ),
-      drawer: Drawer(
-          backgroundColor: Colors.blueGrey,
-          width: 250,
-          child: ListView(
-            children: [
-              DrawerHeader(
-                child: Column(
-                  children: [
-                    CircleAvatar(
-                      radius: 30,
-                      backgroundColor: Colors.blue,
-                    ),
-                    Text(
-                      "Drayer Heder",
-                      style: TextStyle(color: Colors.black, fontSize: 20),
-                    ),
-                  ],
-                ),
-                decoration: BoxDecoration(
-                    borderRadius: BorderRadius.only(
-                        bottomLeft: Radius.circular(100),
-                        bottomRight: Radius.circular(100)),
-                    color: Colors.greenAccent),
-              ),
-              ListTile(
-                leading: Icon(
-                  Icons.settings,
-                  color: Colors.greenAccent,
-                  size: 40,
-                ),
-                title: Text(
-                  "Setings",
-                  style: TextStyle(color: Colors.white, fontSize: 25),
-                ),
-              ),
-              Divider(
-                height: 1.5,
-                color: Colors.white,
-              ),
-              ListTile(
-                leading: Icon(
-                  Icons.person,
-                  color: Colors.greenAccent,
-                  size: 40,
-                ),
-                title: Text(
-                  "Person",
-                  style: TextStyle(color: Colors.white, fontSize: 25),
-                ),
-              ),
-              Divider(
-                height: 1.5,
-                color: Colors.white,
-              ),
-              ListTile(
-                leading: Icon(
-                  Icons.account_box,
-                  color: Colors.greenAccent,
-                  size: 40,
-                ),
-                title: Text(
-                  "Account_box",
-                  style: TextStyle(color: Colors.white, fontSize: 25),
-                ),
-              ),
-              Divider(
-                height: 1.5,
-                color: Colors.white,
-              ),
-              ListTile(
-                leading: Icon(
-                  Icons.logout,
-                  color: Colors.greenAccent,
-                  size: 40,
-                ),
-                title: Text(
-                  "Logout",
-                  style: TextStyle(color: Colors.white, fontSize: 25),
-                ),
-              ),
-              Divider(
-                height: 1.5,
-                color: Colors.white,
-              ),
-            ],
-          )),
+      drawer: Drawer(),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text("Welcome",style: TextStyle(fontSize: 30,fontWeight: FontWeight.bold),),
-            ElevatedButton(
-                onPressed: () {
-                  Navigator.push(context,
-                      MaterialPageRoute(builder: (context) => home_screen()));
-                },
-                child: Text("Next")),
-            SizedBox(height: 10,),
             Text(
-              "counter : 0",
+              "Counter : $c",
               style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
             ),
             ElevatedButton(
-                onPressed: () {},
+                onPressed: () {
+                  setState(() {
+                    counter();
+                  });
+                },
                 child: Text(
-                  "count",
+                  "Count",
                   style: TextStyle(fontWeight: FontWeight.bold),
-                ))
+                )),
           ],
         ),
       ),
