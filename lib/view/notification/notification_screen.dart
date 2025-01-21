a5ca88d14a4d1e393071e9436d85d088c3cffd95
Md Rifat Alam <mdrftlm@gmail.com>
2025-01-21 20:20:23 +0600

    nitification

--- lib/view/notification/notification_screen.dart
+++ lib/view/notification/notification_screen.dart
@@ -7,57 +7,358 @@ class NotificationScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(),
-        body: Column(
-          children: [
-            ListTile(
-              leading: Icon(
-                Icons.menu,
-                size: 30,
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              ListTile(
+                leading: Icon(
+                  Icons.menu,
+                  size: 30,
+                ),
+                title: Text(
+                  "Notifications",
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                ),
+                trailing: Icon(
+                  Icons.notifications_none,
+                  size: 30,
+                ),
               ),
-              title: Text(
-                "Notifications",
-                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-              ),
-              trailing: Icon(
-                Icons.notifications_none,
-                size: 30,
-              ),
-            ),
-            Container(
-              height: 100,
-              width: 300,
-              decoration: BoxDecoration(
-                  color: Colors.blue,
-                  borderRadius: BorderRadius.circular(10)),
-              child: Row(
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Container(
-                      height: 60,
-                      width: 60,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(25),
-                        //color: Colors.white,
-                        image: DecorationImage(
-                            image: AssetImage("assets/image/my_pic.jpg"),
-                            fit: BoxFit.fill),
+              Container(
+                height: 100,
+                width: 300,
+                decoration: BoxDecoration(
+                    color: Colors.white70,
+                    borderRadius: BorderRadius.circular(10)),
+                child: Row(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Container(
+                        height: 60,
+                        width: 60,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(25),
+                          //color: Colors.white,
+                          image: DecorationImage(
+                              image: AssetImage("assets/image/my_pic.jpg"),
+                              fit: BoxFit.fill),
+                        ),
                       ),
                     ),
-                  ),
-                  Text(
-                    "Dont forget to complete\n your task today",
-                    style: TextStyle(fontWeight: FontWeight.bold,),
-                  ),
-                  Stack(
-                    alignment: Alignment.topRight,
+                    Text(
+                      "Dont forget to complete\n your task today",
+                      style: TextStyle(fontWeight: FontWeight.bold,),
+                    ),
+                    Column(
+                     // mainAxisAlignment: MainAxisAlignment.end,
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                      children: [
+                        Icon(Icons.ac_unit_outlined),
+                        SizedBox(height: 50,),
+                        Text("07:45 pm")
+                      ],
+                    ),
+               ] ),
+              ),
+              Container(
+                height: 100,
+                width: 300,
+                decoration: BoxDecoration(
+                    color: Colors.white70,
+                    borderRadius: BorderRadius.circular(10)),
+                child: Row(
                     children: [
-                      Icon(Icons.ac_unit_outlined),
-                    ],
-                  ),
-             ] ),
-            ),
-          ],
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Container(
+                          height: 60,
+                          width: 60,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(25),
+                            //color: Colors.white,
+                            image: DecorationImage(
+                                image: AssetImage("assets/image/my_pic.jpg"),
+                                fit: BoxFit.fill),
+                          ),
+                        ),
+                      ),
+                      Text(
+                        "Dont forget to complete\n your task today",
+                        style: TextStyle(fontWeight: FontWeight.bold,),
+                      ),
+                      Column(
+                        // mainAxisAlignment: MainAxisAlignment.end,
+                        crossAxisAlignment: CrossAxisAlignment.end,
+                        children: [
+                          Icon(Icons.ac_unit_outlined),
+                          SizedBox(height: 50,),
+                          Text("07:45 pm")
+                        ],
+                      ),
+                    ] ),
+              ),
+              Container(
+                height: 100,
+                width: 300,
+                decoration: BoxDecoration(
+                    color: Colors.white70,
+                    borderRadius: BorderRadius.circular(10)),
+                child: Row(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Container(
+                          height: 60,
+                          width: 60,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(25),
+                            //color: Colors.white,
+                            image: DecorationImage(
+                                image: AssetImage("assets/image/my_pic.jpg"),
+                                fit: BoxFit.fill),
+                          ),
+                        ),
+                      ),
+                      Text(
+                        "Dont forget to complete\n your task today",
+                        style: TextStyle(fontWeight: FontWeight.bold,),
+                      ),
+                      Column(
+                        // mainAxisAlignment: MainAxisAlignment.end,
+                        crossAxisAlignment: CrossAxisAlignment.end,
+                        children: [
+                          Icon(Icons.ac_unit_outlined),
+                          SizedBox(height: 50,),
+                          Text("07:45 pm")
+                        ],
+                      ),
+                    ] ),
+              ),
+              Container(
+                height: 100,
+                width: 300,
+                decoration: BoxDecoration(
+                    color: Colors.white70,
+                    borderRadius: BorderRadius.circular(10)),
+                child: Row(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Container(
+                          height: 60,
+                          width: 60,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(25),
+                            //color: Colors.white,
+                            image: DecorationImage(
+                                image: AssetImage("assets/image/my_pic.jpg"),
+                                fit: BoxFit.fill),
+                          ),
+                        ),
+                      ),
+                      Text(
+                        "Dont forget to complete\n your task today",
+                        style: TextStyle(fontWeight: FontWeight.bold,),
+                      ),
+                      Column(
+                        // mainAxisAlignment: MainAxisAlignment.end,
+                        crossAxisAlignment: CrossAxisAlignment.end,
+                        children: [
+                          Icon(Icons.ac_unit_outlined),
+                          SizedBox(height: 50,),
+                          Text("07:45 pm")
+                        ],
+                      ),
+                    ] ),
+              ),
+              Container(
+                height: 100,
+                width: 300,
+                decoration: BoxDecoration(
+                    color: Colors.white70,
+                    borderRadius: BorderRadius.circular(10)),
+                child: Row(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Container(
+                          height: 60,
+                          width: 60,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(25),
+                            //color: Colors.white,
+                            image: DecorationImage(
+                                image: AssetImage("assets/image/my_pic.jpg"),
+                                fit: BoxFit.fill),
+                          ),
+                        ),
+                      ),
+                      Text(
+                        "Dont forget to complete\n your task today",
+                        style: TextStyle(fontWeight: FontWeight.bold,),
+                      ),
+                      Column(
+                        // mainAxisAlignment: MainAxisAlignment.end,
+                        crossAxisAlignment: CrossAxisAlignment.end,
+                        children: [
+                          Icon(Icons.ac_unit_outlined),
+                          SizedBox(height: 50,),
+                          Text("07:45 pm")
+                        ],
+                      ),
+                    ] ),
+              ),
+              Container(
+                height: 100,
+                width: 300,
+                decoration: BoxDecoration(
+                    color: Colors.white70,
+                    borderRadius: BorderRadius.circular(10)),
+                child: Row(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Container(
+                          height: 60,
+                          width: 60,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(25),
+                            //color: Colors.white,
+                            image: DecorationImage(
+                                image: AssetImage("assets/image/my_pic.jpg"),
+                                fit: BoxFit.fill),
+                          ),
+                        ),
+                      ),
+                      Text(
+                        "Dont forget to complete\n your task today",
+                        style: TextStyle(fontWeight: FontWeight.bold,),
+                      ),
+                      Column(
+                        // mainAxisAlignment: MainAxisAlignment.end,
+                        crossAxisAlignment: CrossAxisAlignment.end,
+                        children: [
+                          Icon(Icons.ac_unit_outlined),
+                          SizedBox(height: 50,),
+                          Text("07:45 pm")
+                        ],
+                      ),
+                    ] ),
+              ),
+              Container(
+                height: 100,
+                width: 300,
+                decoration: BoxDecoration(
+                    color: Colors.white70,
+                    borderRadius: BorderRadius.circular(10)),
+                child: Row(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Container(
+                          height: 60,
+                          width: 60,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(25),
+                            //color: Colors.white,
+                            image: DecorationImage(
+                                image: AssetImage("assets/image/my_pic.jpg"),
+                                fit: BoxFit.fill),
+                          ),
+                        ),
+                      ),
+                      Text(
+                        "Dont forget to complete\n your task today",
+                        style: TextStyle(fontWeight: FontWeight.bold,),
+                      ),
+                      Column(
+                        // mainAxisAlignment: MainAxisAlignment.end,
+                        crossAxisAlignment: CrossAxisAlignment.end,
+                        children: [
+                          Icon(Icons.ac_unit_outlined),
+                          SizedBox(height: 50,),
+                          Text("07:45 pm")
+                        ],
+                      ),
+                    ] ),
+              ),
+              Container(
+                height: 100,
+                width: 300,
+                decoration: BoxDecoration(
+                    color: Colors.white70,
+                    borderRadius: BorderRadius.circular(10)),
+                child: Row(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Container(
+                          height: 60,
+                          width: 60,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(25),
+                            //color: Colors.white,
+                            image: DecorationImage(
+                                image: AssetImage("assets/image/my_pic.jpg"),
+                                fit: BoxFit.fill),
+                          ),
+                        ),
+                      ),
+                      Text(
+                        "Dont forget to complete\n your task today",
+                        style: TextStyle(fontWeight: FontWeight.bold,),
+                      ),
+                      Column(
+                        // mainAxisAlignment: MainAxisAlignment.end,
+                        crossAxisAlignment: CrossAxisAlignment.end,
+                        children: [
+                          Icon(Icons.ac_unit_outlined),
+                          SizedBox(height: 50,),
+                          Text("07:45 pm")
+                        ],
+                      ),
+                    ] ),
+              ),
+              Container(
+                height: 100,
+                width: 300,
+                decoration: BoxDecoration(
+                    color: Colors.white70,
+                    borderRadius: BorderRadius.circular(10)),
+                child: Row(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Container(
+                          height: 60,
+                          width: 60,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(25),
+                            //color: Colors.white,
+                            image: DecorationImage(
+                                image: AssetImage("assets/image/my_pic.jpg"),
+                                fit: BoxFit.fill),
+                          ),
+                        ),
+                      ),
+                      Text(
+                        "Dont forget to complete\n your task today",
+                        style: TextStyle(fontWeight: FontWeight.bold,),
+                      ),
+                      Column(
+                        // mainAxisAlignment: MainAxisAlignment.end,
+                        crossAxisAlignment: CrossAxisAlignment.end,
+                        children: [
+                          Icon(Icons.ac_unit_outlined),
+                          SizedBox(height: 50,),
+                          Text("07:45 pm")
+                        ],
+                      ),
+                    ] ),
+              ),
+            ],
+          ),
         ));
   }
 }
