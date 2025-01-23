6f0bfd2425fad43dce9b57d6c0bacd08773d04c5
Md Rifat Alam <mdrftlm@gmail.com>
2025-01-23 20:09:12 +0600

    ListItemAdd

--- lib/view/List/Add_item_to_list.dart
+++ lib/view/List/Add_item_to_list.dart
@@ -21,19 +21,42 @@ class _ListItemAddState extends State<ListItemAdd> {
             padding: const EdgeInsets.all(20.0),
             child: TextField(
               decoration: InputDecoration(
-                enabledBorder: OutlineInputBorder(
+                  enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(30),
+                      borderSide: BorderSide(color: Colors.grey, width: 3)),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Colors.blue,
+                      width: 3,
+                    ),
                     borderRadius: BorderRadius.circular(30),
-                    borderSide: BorderSide(color: Colors.grey, width: 3)),
-                focusedBorder: OutlineInputBorder(
-                  borderSide: BorderSide(
-                    color: Colors.blue,width: 3,
-                  ),borderRadius: BorderRadius.circular(30),
-                )
+                  )),
+            ),
+          ),
+          Card(
+            color: Colors.blue,
+            child: Padding(
+              padding: const EdgeInsets.all(12.0),
+              child: Text(
+                "Add to List",
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
               ),
             ),
-          )
+          ),
+          Divider(
+            thickness: 3,
+          color: Colors.grey,),
+
         ],
       ),
     );
   }
 }
+
+
+
+
+
+
+
+
