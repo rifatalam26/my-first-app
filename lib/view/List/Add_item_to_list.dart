f9e288e951a7b2b5605cc81ac988254277a2889f
Md Rifat Alam <mdrftlm@gmail.com>
2025-01-23 19:55:27 +0600

    ListItemAdd

--- lib/view/List/Add_item_to_list.dart
+++ lib/view/List/Add_item_to_list.dart
@@ -1,32 +1,39 @@
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-    class ListItemAdd extends StatefulWidget {
-      const ListItemAdd({super.key});
-    
-      @override
-      State<ListItemAdd> createState() => _ListItemAddState();
-    }
-    
-    class _ListItemAddState extends State<ListItemAdd> {
-      @override
-      Widget build(BuildContext context) {
-        return Scaffold(
-          appBar: AppBar(
-            backgroundColor: Colors.blue,
-          ),
-          body: Column(
-            children: [
-             TextField(
+class ListItemAdd extends StatefulWidget {
+  const ListItemAdd({super.key});
 
-             )
-            ],
-          ),
-        );
-      }
-    }
+  @override
+  State<ListItemAdd> createState() => _ListItemAddState();
+}
 
-
-
-
+class _ListItemAddState extends State<ListItemAdd> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blue,
+      ),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: TextField(
+              decoration: InputDecoration(
+                enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(30),
+                    borderSide: BorderSide(color: Colors.grey, width: 3)),
+                focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide(
+                    color: Colors.blue,width: 3,
+                  ),borderRadius: BorderRadius.circular(30),
+                )
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
