9cad9d700457d663a6d693d395dea2345af4871f
Md Rifat Alam <mdrftlm@gmail.com>
2025-01-23 21:50:01 +0600

    ListItemAdd

--- lib/view/List/Add_item_to_list.dart
+++ lib/view/List/Add_item_to_list.dart
@@ -9,59 +9,73 @@ class ListItemAdd extends StatefulWidget {
 }
 
 class _ListItemAddState extends State<ListItemAdd> {
+  TextEditingController datacontroller=TextEditingController();
+
+  List<String> list=[];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.blue,
       ),
-      body: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(20.0),
-            child: TextField(
-              decoration: InputDecoration(
-                  enabledBorder: OutlineInputBorder(
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: TextField(
+                controller: datacontroller,
+                decoration: InputDecoration(
+                    enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(30),
+                        borderSide: BorderSide(color: Colors.grey, width: 3)),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.blue,
+                        width: 3,
+                      ),
                       borderRadius: BorderRadius.circular(30),
-                      borderSide: BorderSide(color: Colors.grey, width: 3)),
-                  focusedBorder: OutlineInputBorder(
-                    borderSide: BorderSide(
-                      color: Colors.blue,
-                      width: 3,
-                    ),
-                    borderRadius: BorderRadius.circular(30),
-                  )),
-            ),
-          ),
-          Card(
-            color: Colors.blue,
-            child: Padding(
-              padding: const EdgeInsets.all(12.0),
-              child: Text(
-                "Add to List",
-                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                    )),
               ),
             ),
-          ),
-          Divider(
-            thickness: 3,
-            color: Colors.grey,
-          ),
-          ListView.builder(
-            itemCount: 1,
-              shrinkWrap: true,
-              itemBuilder: (context, index) {
-            return Center(
+            GestureDetector(
+              onTap: (){
+               setState(() {
+                 list.add(datacontroller.text);
+               });
+              },
               child: Card(
                 color: Colors.blue,
                 child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Text("Rifat"),
+                  padding: const EdgeInsets.all(12.0),
+                  child: Text(
+                    "Add to List",
+                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                  ),
                 ),
               ),
-            );
-          })
-        ],
+            ),
+            Divider(
+              thickness: 3,
+              color: Colors.grey,
+            ),
+            ListView.builder(
+              itemCount: list.length,
+                shrinkWrap: true,
+                itemBuilder: (context, index) {
+              return Center(
+                child: Card(
+                  color: Colors.blue,
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(list[index]),
+                  ),
+                ),
+              );
+            })
+          ],
+        ),
       ),
     );
   }
