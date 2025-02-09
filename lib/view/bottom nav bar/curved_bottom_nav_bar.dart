ee50967510cdf74c6e8020b4d0c09b0152ccc770
Md Rifat Alam <mdrftlm@gmail.com>
2025-02-09 22:51:00 +0600

    cruved bottom nav bar

--- lib/view/bottom nav bar/curved_bottom_nav_bar.dart
+++ lib/view/bottom nav bar/curved_bottom_nav_bar.dart
@@ -9,7 +9,7 @@ class CurvedBottomNavBarScreen extends StatefulWidget {
 }
 
 class _CurvedBottomNavBarScreenState extends State<CurvedBottomNavBarScreen> {
-
+  var index=2;
   var items=<Widget>[
     Icon(Icons.home,size: 30,),
     Icon(Icons.favorite,size: 30,),
@@ -20,12 +20,30 @@ class _CurvedBottomNavBarScreenState extends State<CurvedBottomNavBarScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      // extendBody: true,
       backgroundColor: Colors.purple,
+       body:
+      // Image.network("https://images.pexels.com/photos/1590549/pexels-photo-1590549.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"),
+      Center(
+        child: Text("$index",style: TextStyle(fontSize: 120,fontWeight: FontWeight.bold),),
+      ),
+
       appBar: AppBar(
         backgroundColor: Colors.blue,
       ),
       bottomNavigationBar: CurvedNavigationBar(
-          items: items
+        buttonBackgroundColor: Colors.blue,
+        color: Colors.yellowAccent,
+        backgroundColor: Colors.transparent,
+        animationDuration: Duration(milliseconds: 300),
+        height: 60,
+        index:index,
+          items: items,
+        onTap: (i){
+          setState(() {
+            index=i;
+          });
+        },
       ),
     );
   }
