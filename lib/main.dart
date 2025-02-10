3185c068324ab9995a3940b77655536dc2fa3bf8
Md Rifat Alam <mdrftlm@gmail.com>
2025-02-10 23:31:40 +0600

    ui

--- lib/main.dart
+++ lib/main.dart
@@ -24,6 +24,7 @@ import 'package:my_app/view/notification/notification_screen.dart';
 import 'package:my_app/view/package/animated_text.dart';
 import 'package:my_app/view/package/image_picker.dart';
 import 'package:my_app/view/setings%20ui/settings_ui.dart';
+import 'package:my_app/view/ui%20design/bus_app_ui.dart';
 import 'package:my_app/view/ui%20design/first_ui.dart';
 import 'package:my_app/view/profile%20ui/profile.dart';
 import 'package:my_app/view/ui%20design/shopping_ui.dart';
@@ -40,7 +41,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return GetMaterialApp(
       debugShowCheckedModeBanner: false,
-      home: TShirtShoppingUiScreen (),
+      home: LoginScreen (),
     );
   }
 }
---
+++ lib/view/ui design/bus_app_ui.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _BusAppUiScreenState();
+}
+
+class _BusAppUiScreenState extends State<LoginScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}
