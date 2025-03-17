ba82cc719085136c1d32bed3aa03e862f62536ce
Md Rifat Alam <mdrftlm@gmail.com>
2025-03-17 19:53:33 +0600

    model screen2

--- lib/view/Model/model_screen2.dart
+++ lib/view/Model/model_screen2.dart
@@ -1,16 +1,31 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class ModelScreen2 extends StatefulWidget {
-  const ModelScreen2({super.key});
+import 'data_model.dart';
 
+class ModelScreen2 extends StatefulWidget {
+   ModelScreen2({super.key,required this.list});
+  List<DataModel> ? list;
   @override
   State<ModelScreen2> createState() => _ModelScreen2State();
+}
+
+class recuired {
 }
 
 class _ModelScreen2State extends State<ModelScreen2> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        foregroundColor: Colors.white,
+        centerTitle: true,
+        backgroundColor: Colors.blue,
+        title: Text(
+          "Model Screen 2",
+          style: TextStyle(fontWeight: FontWeight.bold),
+        ),
+      ),
+    );
   }
 }
