ae37f6c0eeaf841761807adbe6714be1fa1a3d7e
Md Rifat Alam <mdrftlm@gmail.com>
2025-03-20 19:38:34 +0600

    model screen2 practice

--- lib/view/Model/model_screen2.dart
+++ lib/view/Model/model_screen2.dart
@@ -1,24 +1,15 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-
-import 'data_model.dart';
+import 'package:my_app/view/Model/data_model.dart';
 
 class ModelScreen2 extends StatefulWidget {
-   ModelScreen2({super.key,required this.list});
+   ModelScreen2({super.key ,required this.list});
   List<DataModel> ? list;
   @override
   State<ModelScreen2> createState() => _ModelScreen2State();
 }
 
-class recuired {
-}
-
 class _ModelScreen2State extends State<ModelScreen2> {
-  @override
-  void initState() {
-    print("List_______: ${widget.list!.length ?? []}");
-    super.initState();
-  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -31,19 +22,64 @@ class _ModelScreen2State extends State<ModelScreen2> {
           style: TextStyle(fontWeight: FontWeight.bold),
         ),
       ),
-      body: ListView.builder(
-        itemCount: widget.list!.length,
-          itemBuilder: (context,index){
-            return Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: ListTile(
-                tileColor: Colors.greenAccent,
-                title: Text("${widget.list![index].name}"),
-                subtitle: Text("${widget.list![index].dep}"),
-                leading: Text("${widget.list![index].roll}"),
-              ),
-            );
-          }),
+      body: ListView.builder(itemBuilder: (context,index){
+        return ListTile();
+      }),
     );
   }
 }
+
+
+
+
+
+
+// import 'package:flutter/cupertino.dart';
+// import 'package:flutter/material.dart';
+//
+// import 'data_model.dart';
+//
+// class ModelScreen2 extends StatefulWidget {
+//    ModelScreen2({super.key,required this.list});
+//   List<DataModel> ? list;
+//   @override
+//   State<ModelScreen2> createState() => _ModelScreen2State();
+// }
+//
+// class recuired {
+// }
+//
+// class _ModelScreen2State extends State<ModelScreen2> {
+//   @override
+//   void initState() {
+//     print("List_______: ${widget.list!.length ?? []}");
+//     super.initState();
+//   }
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         foregroundColor: Colors.white,
+//         centerTitle: true,
+//         backgroundColor: Colors.blue,
+//         title: Text(
+//           "Model Screen 2",
+//           style: TextStyle(fontWeight: FontWeight.bold),
+//         ),
+//       ),
+//       body: ListView.builder(
+//         itemCount: widget.list!.length,
+//           itemBuilder: (context,index){
+//             return Padding(
+//               padding: const EdgeInsets.all(8.0),
+//               child: ListTile(
+//                 tileColor: Colors.greenAccent,
+//                 title: Text("${widget.list![index].name}"),
+//                 subtitle: Text("${widget.list![index].dep}"),
+//                 leading: Text("${widget.list![index].roll}"),
+//               ),
+//             );
+//           }),
+//     );
+//   }
+// }
