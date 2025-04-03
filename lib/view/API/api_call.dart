05c5e7e6944bbaeb147b9a2139395dc7aea9d99f
Md Rifat Alam <mdrftlm@gmail.com>
2025-04-03 23:43:10 +0600

    Json decode with for loop

--- lib/view/API/api_call.dart
+++ lib/view/API/api_call.dart
@@ -1,15 +1,18 @@
-
 import 'dart:convert';
 
 import 'package:http/http.dart' as http;
-class ApiCall{
 
-void countryData()async{
-String url ="https://api.nationalize.io/?name=alam";
-  var response = await http.get(Uri.parse(url));
-  // print("Responce : ${response.statusCode}");
-  var data = jsonDecode(response.body);
-  print("------------Data: ${data["name"]}");
-}
-
+class ApiCall {
+  void countryData() async {
+    String url = "https://api.nationalize.io/?name=alam";
+    var response = await http.get(Uri.parse(url));
+    // print("Responce : ${response.statusCode}");
+    var decodedata = jsonDecode(response.body);
+    var data = decodedata["country"];
+    if (response.statusCode == 200) {
+      for (int i = 0; i < data.length; i++) {
+        print("Data : ${data[i]["probability"]}");
+      }
+    }
+  }
 }
