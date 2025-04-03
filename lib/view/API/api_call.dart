077ae60ad356b0d8117e7ef46866b320767184c7
Md Rifat Alam <mdrftlm@gmail.com>
2025-04-03 23:13:37 +0600

    Json decode

--- lib/view/API/api_call.dart
+++ lib/view/API/api_call.dart
@@ -1,11 +1,15 @@
 
+import 'dart:convert';
+
 import 'package:http/http.dart' as http;
 class ApiCall{
 
 void countryData()async{
-String url ="https://api.nationalize.io/?name=nathaniel";
+String url ="https://api.nationalize.io/?name=alam";
   var response = await http.get(Uri.parse(url));
-  print("Responce : ${response.statusCode}");
+  // print("Responce : ${response.statusCode}");
+  var data = jsonDecode(response.body);
+  print("------------Data: ${data["name"]}");
 }
 
 }
