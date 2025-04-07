f067fe81e8c73b73a299ed8fdc597fb45bbc66d0
Md Rifat Alam <mdrftlm@gmail.com>
2025-04-07 19:39:06 +0600

    decode data print

--- lib/view/API/Practice API/class_page.dart
+++ lib/view/API/Practice API/class_page.dart
@@ -1,20 +1,17 @@
-
 import 'dart:convert';
 
 import 'package:http/http.dart' as http;
+
 class api {
+  void Data() async {
+    String url = "https://ipinfo.io/161.185.160.93/geo";
+    var responce = await http.get(Uri.parse(url));
+    var decodedata = jsonDecode(responce.body);
 
-  void Data()async{
-
-    String url="https://ipinfo.io/161.185.160.93/geo";
-    var responce= await http.get(Uri.parse(url));
-    var decodedata= jsonDecode(responce.body);
-    
-    if(responce.statusCode ==200){
-
+    if (responce.statusCode == 200) {
+      print("${decodedata["city"]}");
+      print("${decodedata["ip"]}");
+      print("${decodedata["region"]}");
     }
-    
   }
-
 }
-
