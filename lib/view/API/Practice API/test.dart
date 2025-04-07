7cd27d485df4ee4fa08e9afe26069fefdb248285
Md Rifat Alam <mdrftlm@gmail.com>
2025-04-07 18:18:01 +0600

    api practice app bar

---
+++ lib/view/API/Practice API/test.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class Test extends StatefulWidget {
+  const Test({super.key});
+
+  @override
+  State<Test> createState() => _TestState();
+}
+
+class _TestState extends State<Test> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blue,
+        title: Text(
+          "API Practice",
+          style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+        ),
+      ),
+    );
+  }
+}
